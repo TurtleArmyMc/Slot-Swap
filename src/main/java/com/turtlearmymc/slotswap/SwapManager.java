417255187b25ff321f4309db8beae2f2540ea6fc
KosmX <kosmx.mc@gmail.com>
2023-07-15 18:15:11 +0200

    Implement swap modes

--- src/main/java/com/turtlearmymc/slotswap/SwapManager.java
+++ src/main/java/com/turtlearmymc/slotswap/SwapManager.java
@@ -1,11 +1,16 @@
 package com.turtlearmymc.slotswap;
 
+import com.turtlearmymc.slotswap.config.SwapConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.slot.SlotActionType;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
@@ -16,6 +21,7 @@ public class SwapManager implements ClientModInitializer {
 	public static final int ROW_COUNT = PlayerInventory.MAIN_SIZE / PlayerInventory.getHotbarSize();
 	private static int currentRow;
 	private static boolean active;
+	public static SwapConfig CONFIG;
 
 	private static @Nullable KeyBinding selectSlotKey = null;
 
@@ -54,6 +60,10 @@ public class SwapManager implements ClientModInitializer {
 		return active ? currentRow : 0;
 	}
 
+	private static ClientPlayerInteractionManager getInteractionManager() {
+		return MinecraftClient.getInstance().interactionManager;
+	}
+
 	public static void scroll(PlayerInventory inv, int rowDelta) {
 		tryStartSwap();
 		final int col = inv.selectedSlot;
@@ -69,16 +79,60 @@ public class SwapManager implements ClientModInitializer {
 		}
 	}
 
+	private static void swapSlots(PlayerInventory inv, int slotA, int slotB) {
+
+		// clickSlot has a weird indexing:
+		// first slot arg is slot index, what is kinda random
+		// https://videa.hu/videok/film-animacio/laputa-az-egi-palota-124-perc-1986-KZZvhnBm2KnCxTNx?start=1920.560579
+
+		// second item is normal slot index
+
+		final int slotAIndex = slotA < 9 ? slotA + PlayerInventory.MAIN_SIZE : slotA;
+		getInteractionManager().clickSlot(0, slotAIndex, slotB, SlotActionType.SWAP, MinecraftClient.getInstance().player);
+	}
+
+
 	public static void tryDoSwap(PlayerInventory inv) {
 		if (!active) return;
 		active = false;
 		if (currentRow == 0) return; // Hotbar selected
-		final int slot = rowColToSlot(currentRow, inv.selectedSlot);
-		MinecraftClient.getInstance().interactionManager.pickFromInventory(slot);
+		switch (CONFIG.swapMode) {
+			case COMPATIBLE -> {
+				final int slot = rowColToSlot(currentRow, inv.selectedSlot);
+				getInteractionManager().pickFromInventory(slot);
+			}
+			case SWAP -> {
+				final int slotA = rowColToSlot(currentRow, inv.selectedSlot);
+				final int slotB = rowColToSlot(0, inv.selectedSlot);
+				swapSlots(inv, slotA, slotB);
+			}
+			case SHIFT -> {
+				final int column = inv.selectedSlot;
+				final int distance = currentRow;
+
+				for (int cycleStart = 0, nMoved = 0; nMoved != ROW_COUNT; cycleStart++) {
+					int i = cycleStart;
+					int displaced = i;
+					do {
+						i += distance;
+						if (i >= ROW_COUNT)
+							i -= ROW_COUNT;
+						swapSlots(inv, rowColToSlot(displaced, column), rowColToSlot(i, column));
+						displaced = i;
+						nMoved++;
+					} while ((i + distance) % ROW_COUNT != cycleStart);
+					nMoved++;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void onInitializeClient() {
+
+		AutoConfig.register(SwapConfig.class, JanksonConfigSerializer::new);
+		CONFIG = AutoConfig.getConfigHolder(SwapConfig.class).get();
+
 		selectSlotKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.slotswap.swap",
 				InputUtil.Type.KEYSYM,
